458c2939dcfce07e64d63868983dd88999d240b7
Boxa <Boxa.shu@gmail.com>
2014-06-23 18:17:22 +0400

    final_to github_1

--- [blockreplace]/My Project/Settings.Designer.vb
+++ [blockreplace]/My Project/Settings.Designer.vb
@@ -63,10 +63,10 @@ Namespace My
      Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()>  _
     Friend Module MySettingsProperty
         
-        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global._blockreplace_.My.MySettings
+        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")> _
+        Friend ReadOnly Property Settings() As Global.blockreplace.My.MySettings
             Get
-                Return Global._blockreplace_.My.MySettings.Default
+                Return Global.blockreplace.My.MySettings.Default
             End Get
         End Property
     End Module
